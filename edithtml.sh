a27f08ab90950b005423c7372cbaec8420ccc08a
Daira Hopwood <daira@jacaranda.org>
2020-02-25 17:05:50 +0000

    Add alt="" for anchor image, since it is "decorative" in the sense of <https://www.w3.org/WAI/tutorials/images/decorative/>.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- edithtml.sh
+++ edithtml.sh
@@ -7,5 +7,5 @@ sed -i.sedbak 's|http://cdn.mathjax.org/mathjax/latest/MathJax.js|https://cdnjs.
 # First put <section id="..."> and <hN>...</hN> on the same line. <https://unix.stackexchange.com/a/337399/82702>
 sed -i.sedbak -n '$!N;s|<section id="\([^"]*\)">\s*\n\s*<h|<section id="\1"><h|;P;D' $@
 
-sed -i.sedbak 's|<section id="\([^"]*\)"><h\([1-9]\)>\([^<]*\)</h\([1-9]\)>|<section id="\1"><h\2><span class="section-heading">\3</span><span class="section-anchor"> <a href="#\1"><img width="24" height="24" src="assets/images/section-anchor.png"></a></span></h\4>|' $@
+sed -i.sedbak 's|<section id="\([^"]*\)"><h\([1-9]\)>\([^<]*\)</h\([1-9]\)>|<section id="\1"><h\2><span class="section-heading">\3</span><span class="section-anchor"> <a href="#\1"><img width="24" height="24" src="assets/images/section-anchor.png" alt=""></a></span></h\4>|' $@
 rm -f *.sedbak
