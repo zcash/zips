36cf64abc3755e5c5bff641aee0ab44913391a2a
Daira Hopwood <daira@jacaranda.org>
2020-02-25 12:43:15 +0000

    edithtml.sh: replace MathJax URL. Regenerate ZIP 1011 HTML.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- edithtml.sh
+++ edithtml.sh
@@ -2,4 +2,5 @@
 
 sed -i.sedbak 's|</head>|<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/style.css"></head>|' $@
 sed -i.sedbak 's|<a href="\([^":]*\).rst">|<a href="\1">|' $@
+sed -i.sedbak 's|http://cdn.mathjax.org/mathjax/latest/MathJax.js|https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js|' $@
 rm -f *.sedbak
