681fbec90523e6b383d4281cc07864d574a6aae5
Daira Hopwood <daira@jacaranda.org>
2020-04-22 18:20:32 +0100

    Upgrade MathJax to v3. fixes #353
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- edithtml.sh
+++ edithtml.sh
@@ -2,7 +2,7 @@
 
 sed -i.sedbak 's|</head>|<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="css/style.css"></head>|' $@
 sed -i.sedbak 's|<a href="\([^":]*\).rst">|<a href="\1">|g' $@
-sed -i.sedbak 's|http://cdn.mathjax.org/mathjax/latest/MathJax.js|https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js|' $@
+sed -i.sedbak 's|http://cdn.mathjax.org/mathjax/latest/MathJax.js|https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js|' $@
 
 # First put <section id="..."> and <hN>...</hN> on the same line. <https://unix.stackexchange.com/a/337399/82702>
 sed -i.sedbak -n '$!N;s|<section id="\([^"]*\)">\s*\n\s*<h|<section id="\1"><h|;P;D' $@
