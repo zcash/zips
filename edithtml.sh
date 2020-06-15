aaf0c04f798c317db731788bda2203df49af9d9f
Daira Hopwood <daira@jacaranda.org>
2020-06-15 13:13:04 +0100

    edithtml.sh: make it work on macOS (by using perl instead of sed). Also add permalink anchors to headers containing <code>...</code>.
    Signed-off-by: Daira Hopwood <daira@jacaranda.org>

--- edithtml.sh
+++ edithtml.sh
@@ -35,8 +35,6 @@ fi
 
 sed -i.sedbak 's|<a href="\([^":]*\).rst">|<a href="\1">|g' $2
 
-# Put <section id="..."> and <hN>...</hN> on the same line. <https://unix.stackexchange.com/a/337399/82702>
-sed -i.sedbak -n '$!N;s|<section id="\([^"]*\)">\s*\n\s*<h|<section id="\1"><h|;P;D' $2
-sed -i.sedbak 's|<section id="\([^"]*\)"><h\([1-9]\)>\([^<]*\)</h\([1-9]\)>|<section id="\1"><h\2><span class="section-heading">\3</span><span class="section-anchor"> <a href="#\1"><img width="24" height="24" src="assets/images/section-anchor.png" alt=""></a></span></h\4>|' $2
+perl -i.sedbak -p0e 's|<section id="([^"]*)">\s*.?\s*<h([1-9])>([^<]*(?:<code>[^<]*</code>[^<]*)?)</h([1-9])>|<section id="\1"><h\2><span class="section-heading">\3</span><span class="section-anchor"> <a href="#\1"><img width="24" height="24" src="assets/images/section-anchor.png" alt=""></a></span></h\4>|g' "$2"
 
 rm -f *.sedbak
